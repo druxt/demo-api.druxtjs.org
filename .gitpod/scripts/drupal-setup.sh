73e889ed244a3cac02245c3c525a24a73b26c1df
Stuart Clark <stu@rtclark.net>
2021-10-23 22:25:51 +0000

    fix(#29): update rap command syntax

--- .gitpod/scripts/drupal-setup.sh
+++ .gitpod/scripts/drupal-setup.sh
@@ -6,6 +6,6 @@ set -eu -o pipefail
 ddev composer install
 ddev drush -y si demo_umami
 ddev drush -y en druxt_umami
-ddev drush rap anonymous 'access druxt resources'
+ddev drush rap anonymous 'access\ druxt\ resources'
 ddev drush config:set jsonapi.settings read_only 0 -y
 ddev drush search-api:index
